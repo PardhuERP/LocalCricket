96dfa191febf1afe5a158b4d3cf83369796abb02
PardhuERP <sunny.pardhu@gmail.com>
2026-01-09 11:37:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 const MATCH_ID = "MATCH_1767874129183";
 const API = "https://script.google.com/macros/s/AKfycbwoc84x0cmXWJ6GHzEae4kTJCMdEyvlK7NKq7m12oE6getykgU0UuUUpc37LZcoCuI/exec";
 
+function loadLiveScore() {
+  fetch(`${API}?action=getLiveState&matchId=${MATCH_ID}`)
+    .then(res => res.json())
+    .then(data => {
+      if (data.status !== "ok") return;
+
+      document.getElementById("score").innerText =
+        `${data.totalRuns} / ${data.wickets}`;
+
+      document.getElementById("overs").innerText =
+        `Overs: ${data.over}.${data.ball}`;
+
+      document.getElementById("striker").innerText = data.strikerId;
+      document.getElementById("nonStriker").innerText = data.nonStrikerId;
+      document.getElementById("bowler").innerText = data.bowlerId;
+      document.getElementById("state").innerText = data.state;
+    });
+}
+
+// auto refresh every 2 seconds
+setInterval(loadLiveScore, 2000);
+loadLiveScore();
+
+
 function addRun(runs) {
   fetch(`${API}?action=addRun&matchId=${MATCH_ID}&runs=${runs}`)
     .then(res => res.json())
